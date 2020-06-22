f3c3d9c6bf3ef8a4f51b4e57f41f10cc13f4fb80
Lesly Juarez <leslyj2@gmail.com>
2020-06-22 14:02:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,12 @@
 
 def key_for_min_value(name_hash)
 
-end
-
+  min_value = nil
+  name_hash.each do |key, value| 
+    if min_value == nil 
+      min_value = key 
+    elsif value.to_i < name_hash[min_value].to_i
+      min_value = key 
+    end 
+  end
+  min_value
